adfc5a0c3d454c483d581eac1fea793b311fcecf
n-isab <nisabmohamed@gmail.com>
2026-04-14 12:05:58 +0530

    Use environment variable for API URL

--- client/src/api/notes.js
+++ client/src/api/notes.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = 'https://keep-notes-c6t6.onrender.com/api' ;
-// import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL ;
+
 const API = axios.create({
   baseURL: API_URL,
 });
